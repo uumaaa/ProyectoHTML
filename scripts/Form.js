b1fda7ffd1daf2b96df2e6262c445a723c3b2c18
uumaaa <ikerantonioplumaamro@gmail.com>
2023-03-01 14:53:04 -0600

    formulario centrado

--- scripts/Form.js
+++ scripts/Form.js
@@ -8,14 +8,14 @@ function register()
     x.style.left="-400px";
     y.style.left="50px";
     z.style.left="110px";  
-    document.getElementById("BotonInicio").style.color = "#80004a";
-    document.getElementById("BotonRegistro").style.color = "#F9EAE1";
+    setTimeout(() => {      document.getElementById("BotonInicio").style.color = "#80004a";
+    document.getElementById("BotonRegistro").style.color = "#F9EAE1"; }, 100);
 }
 function login()
 {
     x.style.left="50px";
     y.style.left="450px";
     z.style.left="0px";
-    document.getElementById("BotonRegistro").style.color = "#80004a";
-    document.getElementById("BotonInicio").style.color = "#F9EAE1";
+    setTimeout(() => {      document.getElementById("BotonRegistro").style.color = "#80004a";
+    document.getElementById("BotonInicio").style.color = "#F9EAE1"; }, 100);
 }
