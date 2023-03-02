62f9d43e929432686e710eda6f6888b959ca64e3
uumaaa <ikerantonioplumaamro@gmail.com>
2023-03-01 20:57:07 -0600

    Formulario con porcentajes

--- scripts/Form.js
+++ scripts/Form.js
@@ -5,17 +5,17 @@ var y=document.getElementById("register");
 var z=document.getElementById("btn");
 function register()
 {
-    x.style.left="-400px";
-    y.style.left="50px";
-    z.style.left="110px";  
+    x.style.left="-600px";
+    y.style.left="46px";
+    z.style.left="50%";  
     setTimeout(() => {      document.getElementById("BotonInicio").style.color = "#80004a";
     document.getElementById("BotonRegistro").style.color = "#F9EAE1"; }, 100);
 }
 function login()
 {
-    x.style.left="50px";
-    y.style.left="450px";
-    z.style.left="0px";
+    x.style.left="46px";
+    y.style.left="600px";
+    z.style.left="0";
     setTimeout(() => {      document.getElementById("BotonRegistro").style.color = "#80004a";
     document.getElementById("BotonInicio").style.color = "#F9EAE1"; }, 100);
 }
